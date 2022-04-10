3258d10e001eb1135d2dae6b71222af5b09a6353
awaxman11 <awaxman11@gmail.com>
2022-04-09 23:01:51 -0400

    Import asset folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../reset.css'
 import '../styles.css'
+import '../asset'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
