67e0fe4b71f1c1c1683825406a338f1232b13566
awaxman11 <awaxman11@gmail.com>
2022-04-09 23:02:37 -0400

    Remove importaing asset from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../reset.css'
 import '../styles.css'
-import '../asset'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
